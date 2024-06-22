e31fa787edf28d1711506d4f709d28d49da12acb
Yudi Candra <yudi.c@mhs.unsyiah.ac.id>
2024-06-22 22:10:51 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,83 @@
+// App.jsx
 import { useState } from 'react'
+import Todos from './components/Todos';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import TodoForm from './components/TodoForm';
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Have lunch with Guru Domba',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      completed: false,
+    },
+  ])
+
+  console.log(todos)
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter(todo => todo.id !== todoId)
+    setTodos(updatedTodos)
+  }
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      {/* Teruskan function addTodo sebagai props */}
+      <TodoForm addTodo={addTodo} /> 
+      <Todos
+        todos={todos}
+        toggleCompleted={toggleCompleted}
+        deleteTodo={deleteTodo}
+      />
     </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
 }
 
 export default App
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react'
+
+const TodoForm = ({ addTodo }) => {
+  const [title, setTitle] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addTodo(title)
+    setTitle('') // Reset title-nya
+  }
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value)
+  }
+  
+  // Periksa apakah function "handleChangeTitle" berfungsi
+  console.log(title)
+
+
+  return (
+    <div style={styles.container}>
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event)
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Add your Todo"
+          style={styles.formInput}
+          onChange={(event) => {
+            handleChangeTitle(event)
+          }}
+          value={title} // Atur nilai dari input sesuai dengan state  "title"
+        />
+        <button style={styles.button}>Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+    marginBottom: '32px',
+  },
+  formInput: {
+    height: '66px',
+    width: '40%',
+    fontSize: '16px',
+    padding: '0 16px',
+  },
+  button: {
+    height: '72px',
+    fontSize: '16px',
+  },
+}
+
+export default TodoForm
