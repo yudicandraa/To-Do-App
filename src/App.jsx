b0d268e7a07698931094091ec48adb263d453944
Yudi Candra <yudi.c@mhs.unsyiah.ac.id>
2024-06-22 21:35:11 +0700

    Add CSS to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
+import Todos from './components/Todos';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Have lunch with Guru Domba',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      completed: false,
+    },
+  ])
+
+  console.log(todos)
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      <Todos todos={todos} />
     </div>
   )
 }
 
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
+}
 export default App
